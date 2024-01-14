09a3901b61f54d78ef097a37c2f2fba4e8ddad02
Isha Jain <ishajain.cv@gmail.com>
2024-01-15 00:47:55 +0530

    made roll btn work new game work

--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-function Die({ value }) {
+function Die({ value, isHeld, holdDice }) {
+	const styles = {
+		backgroundColor: isHeld ? "#59E391" : "white",
+	};
 	return (
-		<div className="die-face">
+		<div className="die-face" style={styles} onClick={holdDice}>
 			<h2 className="die-num">{value}</h2>
 		</div>
 	);
